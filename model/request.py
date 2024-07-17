9219e9de4d81645edb98caf55c5188d71ccde5f6
HyeongJun Park <khuphj@gmail.com>
2024-07-17 17:59:00 +0900

    Modify: request example 문법수정
    - pydantic V2 에 맞춰서 문서화 | 기본값 구분
    이게 변수옆에 적을수도있는데 model_config를 따로 두고 적어주는게 공식문서에서 권장하는 방법이라고함
    
    전자의 방법으로는 아래의 schem쪽에 값이 뜨는 경우가 있어서 편하긴한데 코드가 간결하고, 뭔가 문서화할때는 저 값이 필요없는데 붙어서 거슬릴거같음 일단 써보자고

--- model/request.py
+++ model/request.py
@@ -1,6 +1,17 @@
 from pydantic import BaseModel
 
 
-class ReqLogin(BaseModel):
-    email: str = "hj.park@caredit.net"
-    password: str = "password"
+class ReqModelLogin(BaseModel):
+    email: str
+    password: str
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "email": "test",
+                    "password": "test",
+                }
+            ]
+        }
+    }
