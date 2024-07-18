ead27def9e306773ba38b29fa60a39c2cf33f12f
HyeongJun Park <khuphj@gmail.com>
2024-07-18 15:23:54 +0900

    ⚡️Add: uvicorn access log 남기는 부분
    - uvicorn.access 제작하려했으나 안됨
    - 그래서 우회적으로 logfile을 이용

--- core/log.py
+++ core/log.py
@@ -8,17 +8,23 @@ def get_logger(path='../logging_config.yaml'):
     with open(path, 'r', encoding='utf-8') as f:
         config = yaml.safe_load(f)
         logging.config.dictConfig(config)
-        return logging.getLogger('app')
+        app_logger = logging.getLogger('app')
+        access_logger = logging.getLogger('app_access')
+        return app_logger, access_logger
+        #return app_logger
 
 
 if __name__ == "__main__":
-    logger = get_logger()
-    #logger.debug('안녕디버그6')
-    logger.info("안녕하세요")
-    #logger.warning('안녕워닝')
-    logger.error('안녕에러')
-    #logger.fatal('안녕파탈')
-    #logger.critical('안녕크리티컬')
-else:
-    logger = get_logger(path=CONFIG.LOG_CONFIG_PATH)
+    logger, access_logger = get_logger(path=CONFIG.LOG_CONFIG_PATH)
     logger.info("Connect: logger + logging_config")
+    logger.info("안녕하세요")
+    logger.error('안녕에러')
+    access_logger.info("어세스로거")
+    #logger.debug('안녕디버그')
+    #logger.warning('안녕워닝')
+    # logger.fatal('안녕파탈')
+    # logger.critical('안녕크리티컬')
+else:
+    logger, access_logger = get_logger(path=CONFIG.LOG_CONFIG_PATH)
+    #logger.info("Connect: logger + logging_config")
+    #access_logger.info('gd')
